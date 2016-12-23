309baa686b2ae92382d650ad3d4839298a0ad3e1
Suri Zhang <surizhang1224@gmail.com>
2016-12-23 09:19:20 -0800

    Add files via upload

---
+++ SimpleLocation.java
@@ -0,0 +1,11 @@
+public class SimpleLocation{
+  // Member variables not shown
+  public SimpleLocation(){ // default constructor, no parameters
+    this. latitude = 32.9;
+    this. longitude = -117.2;
+  }
+  public SimpleLocation(double lat, double lon){ // parameter constructor
+    this. latitude = lat;
+    this. longitude = lon;
+  }
+}
